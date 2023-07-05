189e73d3ddfa757e07a252816cbfc5455327f0cc
isagunenc <yeay347@gmail.com>
2023-07-05 18:31:11 +0300

    oyun
    oyun

---
+++ Assets/kodlar/itemkodları/Kullanılabiliritem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/tahtavekilic")]
+public class Kullanılabiliritem : Temelitem
+{
+    
+}
